ee6b1b6f94016c636d58c9c796cd346c9aea35e8
Mark Rendle <mark@markrendle.net>
2013-07-18 18:05:21 +0100

    More UI-Router definitions

--- angular-ui.d.ts
+++ angular-ui.d.ts
@@ -11,49 +11,67 @@
 ///////////////////////////////////////////////////////////////////////////////
 declare module ng.ui {
 
-    interface IDialogOptions {
-        controller: string;
-        template?: string;
-        templateUrl?: string;
-        backdrop?: bool;
-        keyboard?: bool;
-        backdropClick?: bool;
-        dialogFade?: bool;
-        backdropFade?: bool;
-        resolve?: any;
-    }
+  interface IDialogOptions {
+    controller: string;
+    template?: string;
+    templateUrl?: string;
+    backdrop?: bool;
+    keyboard?: bool;
+    backdropClick?: bool;
+    dialogFade?: bool;
+    backdropFade?: bool;
+    resolve?: any;
+  }
 
-    interface IMessageBoxButtons {
-        label: string;
-        result: string;
-        cssClass?: string;
-    }
+  interface IMessageBoxButtons {
+    label: string;
+    result: string;
+    cssClass?: string;
+  }
 
-    interface IDialogProvider {
-        dialog(opts: IDialogOptions): IDialog;
-        messageBox(title: string, msg: string, btns: IMessageBoxButtons[]): IDialog;
-    }
+  interface IDialogProvider {
+    dialog(opts: IDialogOptions): IDialog;
+    messageBox(title: string, msg: string, btns: IMessageBoxButtons[]): IDialog;
+  }
 
-    interface IDialog {
-        open(): ng.IPromise;
-        close(result: any): void;
-    }
-  
+  interface IDialog {
+    open(): ng.IPromise;
+    close(result: any): void;
+  }
+
   interface IStateConfig {
-    templateUrl: string;
+    template?: string;
+    templateUrl?: string;
+    templateProvider?: Function;
     controller?: any;
     url?: string;
     parent?: IStateConfig;
+    resolve?: any;
+    params?: any[];
+    views?: any;
+    abstract?: bool;
+    onEnter?: Function;
+    onExit?: Function;
+    data?: any;
   }
-    
+
   interface IStateProvider {
     state(name: string, config: IStateConfig): IStateProvider;
   }
-  
+
   interface IState {
     params: any;
     transitionTo(state: string, params?: any, updateLocation?: bool): void;
     transitionTo(state: IStateConfig, params?: any, updateLocation?: bool): void;
     href(state: IStateConfig, params?: any): string;
+    href(stateName: string, params?: any): string;
+    includes(partialStateName: string): bool;
+    is(stateName: string): bool;
+    is(state: IStateConfig): bool;
+    current: IStateConfig;
+  }
+
+  interface IStateParams {
+    [key: string]: any;
   }
 }
